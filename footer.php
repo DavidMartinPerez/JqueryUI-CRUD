4842fbba06fc4332a94efc9f6203bdc3fccd2012
David Martín Pérez <davidmartinperez1@gmail.com>
2018-02-18 19:52:35 +0100

    añadir funcionalidades
    funciones BORRAR, AÑADIR, MODFICAR

---
+++ footer.php
@@ -0,0 +1,37 @@
+<!-- footer -->
+<footer class="page-footer" style="margin-top: 70px">
+  <div class="container">
+    <div class="row">
+      <div class="col l6 s12">
+        <h5 class="white-text">Trabajo para la asignatura de Enterno Cliente</h5>
+        <p class="grey-text text-lighten-4">Usado como framework CSS -> Materializecss</p>
+        <p class="grey-text text-lighten-4">Usando Jquery Ajax</p>
+        <p class="grey-text text-lighten-4">Usado PHP - Mysqli</p>
+        <p class="grey-text text-lighten-4">Usado Jquery UI</p>
+        <p class="grey-text text-lighten-4">Usado Jquery UI - Dialog</p>
+        <p class="grey-text text-lighten-4">Usado Jquery UI - Datepicker</p>
+        <p class="grey-text text-lighten-4">Usado Jquery UI - Validate</p>
+      </div>
+      <div class="col l4 offset-l2 s12">
+        <h5 class="white-text">Funcionalidades</h5>
+        <ul>
+            <li>La página es una administracion de Versiones de videojuegos en la que podemos selecionar un juego de la BBDD, plataforma de la BBDD,
+            fecha, stock, precio, Distribuidora que lo reparte de la BBDD.<li>
+            <li class="black-text">La función de eliminar te pide confirmación por un modal y lo elimina con un FadeOut<li>
+            <li class="">La funcion crear crea un campo para rellenar en la tabla</li>
+            <li class="black-text">La función editar sale un modal para modificar los campos y se sobre escriben en la tabla sin recargar.</li>
+        </ul>
+        <h5 class="white-text">Links de descargas</h5>
+        <ul>
+          <li><a class="grey-text text-lighten-3" href="http://materializecss.com/">MaterializeCSS</a></li>
+          <li><a class="grey-text text-lighten-3" href="https://jqueryui.com/">Jquery UI</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="footer-copyright">
+    <div class="container">
+    © 2018 David Martín Pérez
+    </div>
+  </div>
+</footer>
