5093ecab59d60e2be80ac7c5628857256d69f1cc
David Martín Pérez <davidmartinperez1@gmail.com>
2018-02-19 10:13:31 +0100

    Añadido colores cuando te equivocas en los formularios

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
       	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 		<script type="text/javascript" src="js/materialize.min.js"></script>
 		<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+		<link rel="stylesheet" href="css/style.css">
 		<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 		<script src="jquery-ui-1.12.1.custom/jquery-ui.js"></script>
 		<script src="js/jquery.validate.js"></script>
